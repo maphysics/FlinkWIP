5db70946a7c2b55a9e51b384077c10f4a9c6ac0f
Gerasim Stanchev <gero@tutuf.com>
2018-12-14 02:03:56 +0200

    Define how a check by id would be performed

---
+++ src/f22/CheckById.java
@@ -0,0 +1,19 @@
+package f22;
+
+import java.util.List;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.common.state.ValueState;
+
+public class CheckById extends RichMapFunction<String, List<String>> {
+	private transient ValueState<List<String>> sum;
+
+    @Override
+    public List<String> map(String input) throws Exception {
+    	// take the current state
+    	List<String> current = sum.value();
+        current.add(input);
+        // update the state
+        sum.update(current);
+        return current;
+    }
+}
