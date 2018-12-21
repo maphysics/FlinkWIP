a13e94a3d1aa0d87638d2bc2b7ea1144446ff16b
Missy <missy@eventador.io:wq>
2018-12-21 13:27:58 -0600

    jar compiles and can run with ../bin/flink run --class f22.FUU target/flink-read-kafka-1.0-SNAPSHOT.jar from flink image

---
+++ src/main/java/f22/FUU.java
@@ -0,0 +1,60 @@
+package f22;
+
+import org.apache.flink.api.common.serialization.AbstractDeserializationSchema;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+import org.apache.flink.util.OutputTag;
+
+public class FUU {
+	public static void main(String[] args) throws Exception {
+		String bootstrap_servers = "***";
+		String truststore_location = "***";
+		String truststore_password = "***";
+		String username = "***";
+		String password = "***";
+		// 
+		String module = "org.apache.kafka.common.security.scram.ScramLoginModule";
+        String jaasConfig = String.format("%s required username=\"%s\" password=\"%s\";", module, username, password);
+		String[] opts = {
+				"--read-topic", "defaultsink",
+				//"--write-topic", "defaultsink",
+				"--bootstrap.servers", bootstrap_servers,
+				"--group.id", "summat",
+				"--producer.sasl.jaas.config", module,
+				"--producer.security.protocol", "SASL_SSL",
+				"--security.protocol", "SASL_SSL",
+			    "--security.inter.broker.protocol", "SASL_SSL",
+			    "--sasl.enabled.mechanisms", "SCRAM-SHA-256",
+			    "--sasl.mechanism.inter.broker.protocol", "SCRAM-SHA-256",
+				"--sasl.mechanism", "SCRAM-SHA-256",
+			    "--sasl.jaas.config", jaasConfig,
+			    "--ssl.truststore.type", "jks",
+			    "--ssl.truststore.location", truststore_location,
+			    "--ssl.truststore.password", truststore_password,
+			    "--auto.offset.reset", "earliest",
+			    "--flink.starting-position", "earliest"
+				};
+		final ParameterTool params = ParameterTool.fromArgs(opts);
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.enableCheckpointing(100);
+        env.getConfig().setGlobalJobParameters(params);
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+
+        AbstractDeserializationSchema<String> deserializationSchema = new MyDeserializationSchema();
+        FlinkKafkaConsumer011<String> consumer = new FlinkKafkaConsumer011<String>(
+                params.getRequired("read-topic"),
+                deserializationSchema,
+                params.getProperties());
+        DataStream<String> mStream = env.addSource(consumer);
+        
+        CheckById cbd = new CheckById();
+        mStream
+        .map(cbd)
+        .print();
+        env.execute("FU");
+	}
+}
---
+++ src/main/java/f22/MyDeserializationSchema.java
@@ -0,0 +1,17 @@
+package f22;
+
+import java.io.IOException;
+
+import org.apache.flink.api.common.serialization.AbstractDeserializationSchema;
+import org.json.JSONObject;
+
+public class MyDeserializationSchema extends AbstractDeserializationSchema<String> {
+
+	@Override
+    public String deserialize(byte[] message) throws IOException {
+		//JSONObject obj = new JSONObject(new String(message, "UTF-8"));
+		//obj.toMap().get("id").toString();
+		//System.out.println(new String(message));
+        return new String(message);
+    }
+}
