73630b8b5c88e2fcef3a40e5ea29c6e1f5f7c9ed
Missy <missy@eventador.io:wq>
2018-12-26 13:03:28 -0600

    clean up

--- src/main/java/f22/FUU.java
+++ src/main/java/f22/FUU.java
@@ -10,30 +10,33 @@ import org.apache.flink.util.OutputTag;
 
 public class FUU {
 	public static void main(String[] args) throws Exception {
-		String bootstrap_servers = "***";
-		String truststore_location = "***";
-		String truststore_password = "***";
-		String username = "***";
-		String password = "***";
+		String bootstrap_servers = "localhost:9092";
+		String sink = "tufuf";
+		// Turning off ssl as local doesn't have it
+		// String truststore_location = "***";
+		// String truststore_password = "***";
+		// String username = "***";
+		// String password = "***";
 		// 
-		String module = "org.apache.kafka.common.security.scram.ScramLoginModule";
-        String jaasConfig = String.format("%s required username=\"%s\" password=\"%s\";", module, username, password);
+		// Turning off jaas as my local doesn't use it, add later if still experiencing issues
+		// String module = "org.apache.kafka.common.security.scram.ScramLoginModule";
+        // String jaasConfig = String.format("%s required username=\"%s\" password=\"%s\";", module, username, password);
 		String[] opts = {
-				"--read-topic", "defaultsink",
+				"--read-topic", sink,
 				//"--write-topic", "defaultsink",
 				"--bootstrap.servers", bootstrap_servers,
 				"--group.id", "summat",
-				"--producer.sasl.jaas.config", module,
-				"--producer.security.protocol", "SASL_SSL",
-				"--security.protocol", "SASL_SSL",
-			    "--security.inter.broker.protocol", "SASL_SSL",
-			    "--sasl.enabled.mechanisms", "SCRAM-SHA-256",
-			    "--sasl.mechanism.inter.broker.protocol", "SCRAM-SHA-256",
-				"--sasl.mechanism", "SCRAM-SHA-256",
-			    "--sasl.jaas.config", jaasConfig,
-			    "--ssl.truststore.type", "jks",
-			    "--ssl.truststore.location", truststore_location,
-			    "--ssl.truststore.password", truststore_password,
+				// "--producer.sasl.jaas.config", module,
+				// "--producer.security.protocol", "SASL_SSL",
+				// "--security.protocol", "SASL_SSL",
+			    // "--security.inter.broker.protocol", "SASL_SSL",
+			    // "--sasl.enabled.mechanisms", "SCRAM-SHA-256",
+			    // "--sasl.mechanism.inter.broker.protocol", "SCRAM-SHA-256",
+				// "--sasl.mechanism", "SCRAM-SHA-256",
+			    // "--sasl.jaas.config", jaasConfig,
+			    // "--ssl.truststore.type", "jks",
+			    // "--ssl.truststore.location", truststore_location,
+			    // "--ssl.truststore.password", truststore_password,
 			    "--auto.offset.reset", "earliest",
 			    "--flink.starting-position", "earliest"
 				};
