2c4c813da43542b94f20451335d1910a3c0ac353
maphysics <doctormissyphd@gmail.com>
2018-12-26 14:56:15 -0600

    packages fixed

--- src/main/java/f22/FUU.java
+++ src/main/java/f22/FUU.java
@@ -1,4 +1,4 @@
-package f22;
+package main.java.f22;
 
 import org.apache.flink.api.common.serialization.AbstractDeserializationSchema;
 import org.apache.flink.api.java.utils.ParameterTool;
@@ -6,37 +6,16 @@ import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
-import org.apache.flink.util.OutputTag;
+// import org.apache.flink.util.OutputTag;
 
 public class FUU {
 	public static void main(String[] args) throws Exception {
 		String bootstrap_servers = "localhost:9092";
 		String sink = "tufuf";
-		// Turning off ssl as local doesn't have it
-		// String truststore_location = "***";
-		// String truststore_password = "***";
-		// String username = "***";
-		// String password = "***";
-		// 
-		// Turning off jaas as my local doesn't use it, add later if still experiencing issues
-		// String module = "org.apache.kafka.common.security.scram.ScramLoginModule";
-        // String jaasConfig = String.format("%s required username=\"%s\" password=\"%s\";", module, username, password);
 		String[] opts = {
 				"--read-topic", sink,
-				//"--write-topic", "defaultsink",
 				"--bootstrap.servers", bootstrap_servers,
 				"--group.id", "summat",
-				// "--producer.sasl.jaas.config", module,
-				// "--producer.security.protocol", "SASL_SSL",
-				// "--security.protocol", "SASL_SSL",
-			    // "--security.inter.broker.protocol", "SASL_SSL",
-			    // "--sasl.enabled.mechanisms", "SCRAM-SHA-256",
-			    // "--sasl.mechanism.inter.broker.protocol", "SCRAM-SHA-256",
-				// "--sasl.mechanism", "SCRAM-SHA-256",
-			    // "--sasl.jaas.config", jaasConfig,
-			    // "--ssl.truststore.type", "jks",
-			    // "--ssl.truststore.location", truststore_location,
-			    // "--ssl.truststore.password", truststore_password,
 			    "--auto.offset.reset", "earliest",
 			    "--flink.starting-position", "earliest"
 				};
