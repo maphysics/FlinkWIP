5a7a160bab002ca52d2c3fa8a56e1293cb90d324
Gerasim Stanchev <gero@tutuf.com>
2019-01-24 20:03:38 +0200

    Expect strings, not Map-like structures

--- src/f22/MyDeserializationSchema.java
+++ src/f22/MyDeserializationSchema.java
@@ -4,15 +4,12 @@ import java.io.IOException;
 
 import org.apache.flink.api.common.serialization.AbstractDeserializationSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.json.JSONObject;
 
 public class MyDeserializationSchema extends AbstractDeserializationSchema<Tuple2<String, String>> {
 	private static final long serialVersionUID = 1L;
 
 	@Override
     public Tuple2<String, String> deserialize(byte[] message) throws IOException {
-		JSONObject obj = new JSONObject(new String(message, "UTF-8"));
-		String sku = obj.toMap().get("sku").toString();
-        return Tuple2.of(null, sku);
+        return Tuple2.of(null, new String(message, "UTF-8"));
     }
 }
