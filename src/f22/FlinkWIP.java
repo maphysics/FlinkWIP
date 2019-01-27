1016b727c59b7e14ead979f92ae404859370b4d0
Gerasim Stanchev <gero@tutuf.com>
2019-01-27 13:25:56 +0200

    Introduce connected streams, sinking in a separate topic

--- src/f22/FlinkWIP.java
+++ src/f22/FlinkWIP.java
@@ -1,12 +1,17 @@
 package f22;
 
 import org.apache.flink.api.common.serialization.AbstractDeserializationSchema;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.ConnectedStreams;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
+import org.json.JSONObject;
 
 public class FlinkWIP {
 	public static void main(String[] args) throws Exception {
@@ -20,8 +25,8 @@ public class FlinkWIP {
         String jaasConfig = String.format("%s required username=\"%s\" password=\"%s\";", module, username, password);
 		String[] opts = {
 				"--skus-topic", "skus",
-				"--read-topic", "defaultsink",
-				//"--write-topic", "defaultsink",
+				"--offers-topic", "offers",
+				"--write-topic", "sink2flink",
 				"--bootstrap.servers", bootstrap_servers,
 				"--group.id", "summat",
 				"--producer.sasl.jaas.config", module,
@@ -37,26 +42,44 @@ public class FlinkWIP {
 			    "--ssl.truststore.password", truststore_password,
 			    "--auto.offset.reset", "earliest",
 			    "--flink.starting-position", "earliest"
-				};
-		final ParameterTool params = ParameterTool.fromArgs(opts);
+		};
+		return ParameterTool.fromArgs(opts);
+	};
+		
+	public static void main(String[] args) throws Exception {
+		//final ParameterTool params = ParameterTool.fromArgs(opts);
+        final ParameterTool params = FlinkWIP.init();
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.enableCheckpointing(100);
         env.getConfig().setGlobalJobParameters(params);
         env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
         AbstractDeserializationSchema<Tuple2<String, String>> deserializationSchema = new MyDeserializationSchema();
         FlinkKafkaConsumer011<Tuple2<String, String>> consumer = new FlinkKafkaConsumer011<Tuple2<String, String>>(
                 params.getRequired("skus-topic"),
                 deserializationSchema,
                 params.getProperties());
+        DataStream<Tuple2<String, String>> idStream = env.addSource(consumer);
 
-        DataStream<Tuple2<String, String>> mStream = env.addSource(consumer);
+        // Create datastream for processing `offers`
+        FlinkKafkaConsumer011<Tuple2<String, JSONObject>> eventConsumer = new FlinkKafkaConsumer011<Tuple2<String, JSONObject>>(
+        	params.getRequired("offers-topic"),
+            new EventDeserializerJSONObject(),
+            params.getProperties());
+        DataStream<Tuple2<String,JSONObject>> eventStream = env.addSource(eventConsumer); 
 
-        mStream
-        .keyBy(0)
-        .map(new MapToSkus())
-        .print();
+        ConnectedStreams<Tuple2<String, String>, Tuple2<String,JSONObject>> colStreams = idStream.connect(eventStream);
+
+        DataStream<String> stream = colStreams
+        .keyBy(0, 0)
+        .flatMap(new CheckEventIds());
+
+        stream.addSink(new FlinkKafkaProducer011<String>(
+                params.getRequired("write-topic"),
+                new SimpleStringSchema(),
+                params.getProperties()));
         env.execute("FlinkWIP");
-	}
+    }
 }
