7208bcc22e92b4af6109cf8f942b73e3f55d4190
Gerasim Stanchev <gero@tutuf.com>
2019-01-29 00:19:36 +0200

    Sync SASL auth configuration

--- src/main/java/f22/FlinkWIP.java
+++ src/main/java/f22/FlinkWIP.java
@@ -1,5 +1,8 @@
 package f22;
 
+import java.util.Properties;
+
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.common.serialization.AbstractDeserializationSchema;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
@@ -14,46 +17,37 @@ import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
 import org.json.JSONObject;
 
 public class FlinkWIP {
-	public static ParameterTool init(){
-	    String bootstrap_servers = "***";
-		String truststore_location = "***";
-		String truststore_password = "***";
-		String username = "***";
-		String password = "***";
-		// 
-		String module = "org.apache.kafka.common.security.scram.ScramLoginModule";
-        String jaasConfig = String.format("%s required username=\"%s\" password=\"%s\";", module, username, password);
-		String[] opts = {
-				"--skus-topic", "skus",
-				"--offers-topic", "offers",
-				"--write-topic", "sink2flink",
-				"--bootstrap.servers", bootstrap_servers,
-				"--group.id", "summat",
-				"--producer.sasl.jaas.config", module,
-				"--producer.security.protocol", "SASL_SSL",
-				"--security.protocol", "SASL_SSL",
-			    "--security.inter.broker.protocol", "SASL_SSL",
-			    "--sasl.enabled.mechanisms", "SCRAM-SHA-256",
-			    "--sasl.mechanism.inter.broker.protocol", "SCRAM-SHA-256",
-				"--sasl.mechanism", "SCRAM-SHA-256",
-			    "--sasl.jaas.config", jaasConfig,
-			    "--ssl.truststore.type", "jks",
-			    "--ssl.truststore.location", truststore_location,
-			    "--ssl.truststore.password", truststore_password,
-			    "--auto.offset.reset", "earliest",
-			    "--flink.starting-position", "earliest"
-		};
-		return ParameterTool.fromArgs(opts);
-	};
-		
 	public static void main(String[] args) throws Exception {
-        final ParameterTool params = FlinkWIP.init();
+		final ParameterTool params = ParameterTool.fromArgs(args);
+		ClassLoader classLoader = FlinkWIP.class.getClassLoader();
+        String truststore_path = classLoader.getResource("eventador_truststore.jks").getPath();
+
+        // Configure ScramLogin via jaas
+        String module = "org.apache.kafka.common.security.scram.ScramLoginModule";
+        String jaasConfig = String.format("%s required username=\"%s\" password=\"%s\";", module, params.getRequired("username"), params.getRequired("password"));
+
+        // Flink and Kafka parameters
+        Properties kparams = params.getProperties();
+        kparams.setProperty("auto.offset.reset", "earliest");
+        kparams.setProperty("flink.starting-position", "earliest");
+        kparams.setProperty("group.id", params.getRequired("group.id"));
+        kparams.setProperty("bootstrap.servers", params.getRequired("bootstrap.servers"));;
+
+        // SASL parameters
+        kparams.setProperty("security.protocol", "SASL_SSL");
+        kparams.setProperty("sasl.mechanism", "SCRAM-SHA-256");
+        kparams.setProperty("sasl.jaas.config", jaasConfig);
+        kparams.setProperty("ssl.truststore.type", "jks");
+        kparams.setProperty("ssl.truststore.location", truststore_path);
+        kparams.setProperty("ssl.truststore.password", params.getRequired("truststore.password"));
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.enableCheckpointing(100);
+        env.getConfig().setRestartStrategy(RestartStrategies.fixedDelayRestart(4, 10000));
+        env.enableCheckpointing(300000);
         env.getConfig().setGlobalJobParameters(params);
         env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        env.setParallelism(1);
 
         AbstractDeserializationSchema<Tuple2<String, String>> deserializationSchema = new MyDeserializationSchema();
         FlinkKafkaConsumer011<Tuple2<String, String>> consumer = new FlinkKafkaConsumer011<Tuple2<String, String>>(
